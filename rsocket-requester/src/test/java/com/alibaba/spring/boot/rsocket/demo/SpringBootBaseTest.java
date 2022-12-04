c0e35c75b1a77ffcf1ee5f6e7ecbfae9257c3031
Libing Chen <libing.chen@gmail.com>
2022-12-04 15:28:05 +0800

    initial version

---
+++ rsocket-requester/src/test/java/com/alibaba/spring/boot/rsocket/demo/SpringBootBaseTest.java
@@ -0,0 +1,26 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import com.alibaba.rsocket.upstream.UpstreamManager;
+import com.alibaba.rsocket.upstream.UpstreamManagerMock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@Import(SpringBootBaseTest.RSocketTestConfig.class)
+@TestPropertySource(properties = {"rsocket.disabled=true"})
+public abstract class SpringBootBaseTest {
+
+    @SuppressWarnings({"deprecation", "rawtypes"})
+    @TestConfiguration
+    static class RSocketTestConfig {
+
+        @Bean
+        public UpstreamManager upstreamManager() {
+            return new UpstreamManagerMock();
+        }
+
+    }
+}
